ea2093602b9a2e4b044fd6e0400296d3469eb04a
liangke <lokyse@163.com>
2017-08-18 21:28:36 +0800

    Add openstack proxy function
    Change-Id: I6b4439df4905a391ebd2e4eec3b8491e0c06ead4
    Issue-Id: MULTICLOUD-55
    Signed-off-by: liangke <lokyse@163.com>

---
+++ vio/vio/swagger/views/proxyplugin/volumn/views.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2017 VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+from vio.swagger.views.proxyplugin.httpclient import BaseClient
+
+
+class VolumeServer(BaseClient):
+
+    serverType = "volume"
+
+    def get(self,request,vimid,tenantid,other):
+
+        return  self.send(request=request,method="GET",vimid=vimid,tenantid=tenantid,other=other)
+
+    def post(self,request,vimid,tenantid,other):
+
+        return  self.send(request=request,method="POST",vimid=vimid,tenantid=tenantid,other=other)
+
+    def put(self, request, vimid, tenantid, other):
+
+        return  self.send(request=request,method="PUT",vimid=vimid,tenantid=tenantid,other=other)
+
+    def delete(self, request, vimid, tenantid, other):
+
+        return  self.send(request=request,method="DELETE",vimid=vimid,tenantid=tenantid,other=other)
+
+    def patch(self, request, vimid, tenantid, other):
+
+        return  self.send(request=request,method="PATCH",vimid=vimid,tenantid=tenantid,other=other)
+
+
+class VolumeServer2(VolumeServer):
+    serverType = "volumev2"
+
+class VolumeServer3(VolumeServer):
+    serverType = "volumev3"
+
+
+
+
+
+
+
+
