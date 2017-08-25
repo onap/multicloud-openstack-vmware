538ecd97f8940f5fcbd7fec4119c4c6fcf3cdffe
liangke <lokyse@163.com>
2017-08-25 10:32:55 +0800

    handle  error respons code
      If response code is not normal, error has
    happened.
    
    Change-Id: I266f3cfb0173189510715db05b28540c4da98ba3
    Issue-Id: MULTICLOUD-55
    Signed-off-by: liangke <lokyse@163.com>

--- vio/vio/swagger/views/proxyplugin/identity/views.py
+++ vio/vio/swagger/views/proxyplugin/identity/views.py
@@ -93,9 +93,12 @@ class TokenView(BaseClient):
         logger.info("vimid(%(vimid)s) get keystone url %(url)s ",
                     {"vimid": vimid, "url": keystoneURL})
         try:
-            res = requests.get(url=keystoneURL).json()
-            res['version']['links'][0]['href'] = "http://" + \
-                MSB_ADDRESS + "/multicloud-vio/v0/" + vimid + "/identity/v3"
+            res = requests.get(url=keystoneURL)
+            if res.status_code != status.HTTP_200_OK:
+                return Response(data={"error":res.content}, status=res.status_code)
+            res = res.json()
+            res['version']['links'][0]['href']="http://"+MSB_ADDRESS+"/multicloud-vio/v0/"+vimid +"/identity/v3"
+
 
         except Exception as e:
             logging.exception("error %s" % e)
@@ -143,8 +146,9 @@ class TokenView(BaseClient):
                     {"vimid": vimid, "url": url})
 
         try:
-            res = requests.post(url=url, data=json.dumps(
-                create_req), headers=headers)
+            res = requests.post(url=url, data=json.dumps(create_req), headers=headers)
+            if res.status_code != status.HTTP_201_CREATED:
+                return Response(data={"error":res.content}, status=res.status_code)
             tokenInfo = res.json()
             resHeader = dict(res.headers)
         except Exception as e:
