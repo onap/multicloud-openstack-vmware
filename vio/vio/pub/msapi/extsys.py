230456db33e911a4f0e1b3e5810198c5cc980948
Ethan Lynn <ethanlynnl@vmware.com>
2017-08-30 16:41:41 +0800

    Add AAI adapter to vio plugin
    Change-Id: I70f1cf0a297883b844689a4ddbc1bfde1f0ed2bc
    issue-id: MULTICLOUD-81
    Signed-off-by: Ethan Lynn <ethanlynnl@vmware.com>

--- vio/vio/pub/msapi/extsys.py
+++ vio/vio/pub/msapi/extsys.py
@@ -10,28 +10,33 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-import json
 import logging
 
-from rest_framework import status
-from vio.pub.exceptions import VimDriverVioException
-from vio.pub.utils.restcall import req_by_msb
+from vio.pub.utils.restcall import AAIClient
 
 logger = logging.getLogger(__name__)
 
 
-def get_vims():
-    ret = req_by_msb("/openoapi/extsys/v1/vims", "GET")
-    if ret[0] != 0:
-        logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
-        raise VimDriverVioException("Failed to query VIMs from extsys.")
-    return json.JSONDecoder().decode(ret[1])
+def split_vim_to_owner_region(vim_id):
+    split_vim = vim_id.split('_')
+    cloud_owner = split_vim[0]
+    cloud_region = "".join(split_vim[1:])
+    return cloud_owner, cloud_region
 
 
 def get_vim_by_id(vim_id):
-    ret = req_by_msb("/openoapi/extsys/v1/vims/%s" % vim_id, "GET")
-    if ret[0] != 0:
-        logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
-        raise VimDriverVioException("Failed to query VIM with \
-            id (%s) from extsys." % vim_id, status.HTTP_404_NOT_FOUND)
-    return json.JSONDecoder().decode(ret[1])
+    cloud_owner, cloud_region = split_vim_to_owner_region(vim_id)
+    client = AAIClient(cloud_owner, cloud_region)
+    ret = client.get_vim(get_all=True)
+    ret['type'] = ret['cloud-type']
+    ret['version'] = ret['cloud-region-version']
+    ret['vimId'] = vim_id
+    ret['userName'] = ret['esr-system-info-list'][
+        'esr-system-info'][0]['username']
+    ret['password'] = ret['esr-system-info-list'][
+        'esr-system-info'][0]['password']
+    ret['tenant'] = ret['esr-system-info-list'][
+        'esr-system-info'][0]['default-tenant']
+    ret['url'] = ret['esr-system-info-list'][
+        'esr-system-info'][0]['service-url']
+    return ret
