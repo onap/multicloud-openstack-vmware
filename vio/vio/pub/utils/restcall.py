8af0790be80a60b74e8b5f4cb954ec6f86736f93
Ethan Lynn <ethanlynnl@vmware.com>
2017-08-30 16:41:41 +0800

    Add AAI adapter to vio plugin
    Change-Id: I70f1cf0a297883b844689a4ddbc1bfde1f0ed2bc
    issue-id: MULTICLOUD-81
    Signed-off-by: Ethan Lynn <ethanlynnl@vmware.com>

--- vio/vio/pub/utils/restcall.py
+++ vio/vio/pub/utils/restcall.py
@@ -16,8 +16,15 @@ import logging
 import urllib2
 import uuid
 import httplib2
+import json
 
+from vio.pub.config.config import AAI_SCHEMA_VERSION
+from vio.pub.config.config import AAI_SERVICE_URL
+from vio.pub.config.config import AAI_USERNAME
+from vio.pub.config.config import AAI_PASSWORD
 from vio.pub.config.config import MSB_SERVICE_IP, MSB_SERVICE_PORT
+
+from vio.pub.exceptions import VimDriverVioException
 
 rest_no_auth, rest_oneway_auth, rest_bothway_auth = 0, 1, 2
 HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_202_ACCEPTED = '200', '201', '204', '202'
@@ -27,15 +34,21 @@ HTTP_404_NOTFOUND, HTTP_403_FORBIDDEN, HTTP_401_UNAUTHORIZED, HTTP_400_BADREQUES
 logger = logging.getLogger(__name__)
 
 
-def call_req(base_url, user, passwd, auth_type, resource, method, content=''):
+def call_req(base_url, user, passwd, auth_type, resource, method, content='', headers=None):
     callid = str(uuid.uuid1())
-    logger.debug("[%s]call_req('%s','%s','%s',%s,'%s','%s','%s')" % (
-        callid, base_url, user, passwd, auth_type, resource, method, content))
+#    logger.debug("[%s]call_req('%s','%s','%s',%s,'%s','%s','%s')" % (
+#        callid, base_url, user, passwd, auth_type, resource, method, content))
     ret = None
     resp_status = ''
+    resp = ""
+    full_url = ""
+
     try:
         full_url = combine_url(base_url, resource)
-        headers = {'content-type': 'application/json', 'accept': 'application/json'}
+        if headers == None:
+            headers = {}
+            headers['content-type']='application/json'
+
         if user:
             headers['Authorization'] = 'Basic ' + ('%s:%s' % (user, passwd)).encode("base64")
         ca_certs = None
@@ -43,35 +56,35 @@ def call_req(base_url, user, passwd, auth_type, resource, method, content=''):
             http = httplib2.Http(ca_certs=ca_certs, disable_ssl_certificate_validation=(auth_type == rest_no_auth))
             http.follow_all_redirects = True
             try:
+                logger.debug("request=%s)" % full_url)
                 resp, resp_content = http.request(full_url, method=method.upper(), body=content, headers=headers)
                 resp_status, resp_body = resp['status'], resp_content.decode('UTF-8')
-                logger.debug("[%s][%d]status=%s,resp_body=%s)" % (callid, retry_times, resp_status, resp_body))
+
                 if resp_status in status_ok_list:
-                    ret = [0, resp_body, resp_status]
+                    ret = [0, resp_body, resp_status, resp]
                 else:
-                    ret = [1, resp_body, resp_status]
+                    ret = [1, resp_body, resp_status, resp]
                 break
             except Exception as ex:
                 if 'httplib.ResponseNotReady' in str(sys.exc_info()):
-                    logger.debug("retry_times=%d", retry_times)
                     logger.error(traceback.format_exc())
-                    ret = [1, "Unable to connect to %s" % full_url, resp_status]
+                    ret = [1, "Unable to connect to %s" % full_url, resp_status, resp]
                     continue
                 raise ex
     except urllib2.URLError as err:
-        ret = [2, str(err), resp_status]
+        ret = [2, str(err), resp_status, resp]
     except Exception as ex:
         logger.error(traceback.format_exc())
         logger.error("[%s]ret=%s" % (callid, str(sys.exc_info())))
         res_info = str(sys.exc_info())
         if 'httplib.ResponseNotReady' in res_info:
             res_info = "The URL[%s] request failed or is not responding." % full_url
-        ret = [3, res_info, resp_status]
+        ret = [3, res_info, resp_status, resp]
     except:
         logger.error(traceback.format_exc())
-        ret = [4, str(sys.exc_info()), resp_status]
+        ret = [4, str(sys.exc_info()), resp_status, resp]
 
-    logger.debug("[%s]ret=%s" % (callid, str(ret)))
+#    logger.debug("[%s]ret=%s" % (callid, str(ret)))
     return ret
 
 
@@ -91,3 +104,179 @@ def combine_url(base_url, resource):
     else:
         full_url = base_url + '/' + resource
     return full_url
+
+
+def get_res_from_aai(resource, content=''):
+    headers = {
+        'X-FromAppId': 'MultiCloud',
+        'X-TransactionId': '9001',
+        'content-type': 'application/json',
+        'accept': 'application/json'
+    }
+    base_url = "%s/%s" % (AAI_SERVICE_URL, AAI_SCHEMA_VERSION)
+    return call_req(base_url, AAI_USERNAME, AAI_PASSWORD, rest_no_auth,
+                    resource, "GET", content, headers)
+
+
+class AAIClient(object):
+    def __init__(self, cloud_owner, cloud_region):
+        self.base_url = "%s/%s" % (AAI_SERVICE_URL, AAI_SCHEMA_VERSION)
+        self.username = AAI_USERNAME
+        self.password = AAI_PASSWORD
+        self.default_headers = {
+            'X-FromAppId': 'multicloud-openstack-vmware',
+            'X-TransactionId': '9004',
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        }
+        self.cloud_owner = cloud_owner
+        self.cloud_region = cloud_region
+
+    def get_vim(self, get_all=False):
+        resource = ("/cloud-infrastructure/cloud-regions/cloud-region"
+                    "/%s/%s" % (self.cloud_owner, self.cloud_region))
+        if get_all:
+            resource = "%s?depth=all" % resource
+        resp = call_req(self.base_url, self.username, self.password,
+                        rest_no_auth, resource, "GET",
+                        headers=self.default_headers)
+        if resp[0] != 0:
+            raise VimDriverVioException(status_code=404, content=
+                "Failed to query VIM with id (%s) from extsys." % vim_id)
+        return json.loads(resp[1])
+
+    def update_vim(self, content):
+        # update identity url
+        self.update_identity_url()
+        # update tenants
+        self.add_tenants(content)
+        # update default tenant
+
+        # update flavors
+        self.add_images(content)
+        # update images
+        self.add_flavors(content)
+        # update pservers
+        self.add_pservers(content)
+
+    def update_identity_url(self):
+        vim = self.get_vim()
+        vim['identity-url'] = ("http://%s/api/multicloud/v0/%s_%s/identity/"
+                               "v3" % (MSB_SERVICE_IP, self.cloud_owner,
+                                       self.cloud_region))
+        resource = ("/cloud-infrastructure/cloud-regions/cloud-region"
+                    "/%s/%s" % (self.cloud_owner, self.cloud_region))
+        resp = call_req(self.base_url, self.username, self.password,
+                        rest_no_auth, resource, "PUT", content=json.dumps(vim)
+                        headers=self.default_headers)
+
+    def add_tenants(self, content):
+        for tenant in content['projects']:
+            resource = ("/cloud-infrastructure/cloud-regions/cloud-region/"
+                        "%s/%s/tenants/tenant/%s" % (
+                        self.cloud_owner, self.cloud_region, tenant['id']))
+            body = {'tenant-name': tenant['name']}
+            resp = call_req(self.base_url, self.username, self.password,
+                            rest_no_auth, resource, "PUT",
+                            content=json.dumps(body)
+                            headers=self.default_headers)
+
+    def add_flavors(self, content):
+        for flavor in content['flavors']:
+            resource = ("/cloud-infrastructure/cloud-regions/cloud-region/"
+                        "%s/%s/flavors/flavor/%s" % (
+                        self.cloud_owner, self.cloud_region, flavor['id']))
+            body = {
+                'flavor-name': flavor['name'],
+                'flavor-vcpus': flavor['vcpus'],
+                'flavor-ram': flavor['ram'],
+                'flavor-disk': flavor['disk'],
+                'flavor-ephemeral': flavor['OS-FLV-EXT-DATA:ephemeral'],
+                'flavor-swap': flavor['swap'],
+                'flavor-is-public': flavor['os-flavor-access:is_public'],
+                'flavor-selflink': flavor['links'][0]['href'],
+                'flavor-disabled': flavor['OS-FLV-DISABLED:disabled']
+            }
+            resp = call_req(self.base_url, self.username, self.password,
+                            rest_no_auth, resource, "PUT",
+                            content=json.dumps(body)
+                            headers=self.default_headers)
+
+    def add_images(self, content):
+        for image in content['images']:
+            resource = ("/cloud-infrastructure/cloud-regions/cloud-region/"
+                        "%s/%s/images/image/%s" % (
+                        self.cloud_owner, self.cloud_region, image['id']))
+            body = {
+                'image-name': image['name'],
+                # 'image-architecture': image[''],
+                'image-os-distro': image['name'].split("-")[0],
+                'image-os-version': image['name'].split("-")[1],
+                # 'application': image[''],
+                # 'application-vendor': image[''],
+                # 'application-version': image[''],
+                'image-selflink': image['self'],
+                }
+            resp = call_req(self.base_url, self.username, self.password,
+                            rest_no_auth, resource, "PUT",
+                            content=json.dumps(body)
+                            headers=self.default_headers)
+
+    def add_pservers(self, content):
+        for hypervisor in content['hypervisors']:
+            resource = ("/cloud-infrastructure/pservers/pserver/%s" % (
+                hypervisor['hypervisor_hostname']))
+            body = {
+                # 'ptnii-equip-name'
+                'number-of-cpus': hypervisor['vcpus'],
+                'disk-in-gigabytes': hypervisor['local_gb'],
+                'ram-in-megabytes': hypervisor['memory_mb'],
+                # 'equip-type'
+                # 'equip-vendor'
+                # 'equip-model'
+                # 'fqdn'
+                # 'pserver-selflink'
+                'ipv4-oam-address': hypervisor['host_ip'],
+                # 'serial-number'
+                # 'ipaddress-v4-loopback-0'
+                # 'ipaddress-v6-loopback-0'
+                # 'ipaddress-v4-aim'
+                # 'ipaddress-v6-aim'
+                # 'ipaddress-v6-oam'
+                # 'inv-status'
+                'pserver-id': hypervisor['service']['id'],
+                # 'internet-topology'
+            }
+            resp = call_req(self.base_url, self.username, self.password,
+                            rest_no_auth, resource, "PUT",
+                            content=json.dumps(body)
+                            headers=self.default_headers)
+            # update relationship
+            resource = ("/cloud-infrastructure/pservers/pserver/%s/"
+                        "relationship-list/relationship" % (
+                        hypervisor['hypervisor_hostname']))
+            related-link = ("%s/cloud-infrastructure/cloud-regions/"
+                            "cloud-region/%s/%s" % (
+                self.base_url, self.cloud_owner, self.cloud_region))
+            body = {
+                'related-to': 'cloud-region',
+                'related-link': related-link,
+                'relationship-data': [
+                    {
+                        'relationship-key': 'cloud-region.cloud-owner',
+                        'relationship-value': self.cloud_owner
+                    },
+                    {
+                        'relationship-key': 'cloud-region.cloud-region-id',
+                        'relationship-value': self.cloud_region
+                    },
+                    {
+                        'relationship-key': 'pserver.hostname',
+                        'relationship-value': hypervisor['hypervisor_hostname']
+                    }
+                ]
+            }
+            resp = call_req(self.base_url, self.username, self.password,
+                            rest_no_auth, resource, "PUT",
+                            content=json.dumps(body)
+                            headers=self.default_headers)
