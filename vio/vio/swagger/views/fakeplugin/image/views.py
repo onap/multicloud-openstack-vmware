5d990f15bd1b007fd6e55eca585da92dc0fe9626
liangke <lokyse@163.com>
2017-09-05 11:46:04 +0800

    Add fake-vio testing
    Change-Id: I9250ac39c46d6576076b98b1048544a103e11597
    Issue-Id: MULTICLOUD-79
    Signed-off-by: liangke <lokyse@163.com>

---
+++ vio/vio/swagger/views/fakeplugin/image/views.py
@@ -0,0 +1,32 @@
+
+from rest_framework import status
+
+from rest_framework.views import APIView
+
+
+
+
+
+
+class FakeImage(APIView):
+
+
+
+    def get(self,other):
+        return
+
+
+    def put(self,other):
+        return
+
+
+    def head(self,other):
+        return
+
+    def post(self,other):
+        return
+
+
+
+    def delete(self,other):
+        return
