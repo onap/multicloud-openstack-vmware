89be39877f523e5f8d55d4c32be0aa2dd175624f
liangke <lokyse@163.com>
2017-09-12 18:42:52 +0800

    Start up VIO server independtly
    It seems that we do not need waiting msb server
    be initilized first during OOM deplpyment.
    
    Change-Id: Ib50d17d0c1291824a71cbae77c0a8156e59b1155
    Issue-Id: MULTICLOUD-74
    Signed-off-by: liangke <lokyse@163.com>

--- vio/docker/docker-entrypoint.sh
+++ vio/docker/docker-entrypoint.sh
@@ -15,12 +15,6 @@ fi
 echo "MSB_ADDR=$MSB_ADDR"
 echo
 
-# Wait for MSB initialization
-echo Wait for MSB initialization
-for i in {1..20}; do
-    curl -sS -m 1 $MSB_ADDR > /dev/null && break
-    sleep $i
-done
 
 echo
 
