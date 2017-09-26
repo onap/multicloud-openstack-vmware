3b5ad66355ece70108d990218dbb6fb027a67f97
Arun Arora <aroraarun@vmware.com>
2017-09-26 21:57:03 +0530

    vesagent review comments incorporated
    Removed trailing spaces
    Replaced TAB with 4 SPACE chars
    
    Issue-Id: MULTICLOUD-8
    
    Change-Id: If4b7f82fe7175a4d018cd5c5232217e98824c0bd
    Signed-off-by: Arun Arora <aroraarun@vmware.com>

---
+++ vesagent/src/main/java/org/onap/multicloud/openstack/vmware/JsonStoreMap.java
@@ -0,0 +1,123 @@
+ /* Copyright (c) 2017 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package org.onap.multicloud.openstack.vmware;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+/*JsonStoreMap class having all methods
+ *  related with HashMap
+ *  Map taking UUID of VM as key
+ *  Value is List Contain encoded json, alarm condition
+ *  and ves send status
+ *  If need to add any values --> add to JsonStoreMap */
+
+public class JsonStoreMap {
+    public static Map<String, List<JsonAlarmStorage>> map = new HashMap<String, List<JsonAlarmStorage>>();
+
+    public Map<String, List<JsonAlarmStorage>> addToMap(String key,List<JsonAlarmStorage> value){
+        System.out.println("adding to map");
+        map.put(key, value);
+        return map;
+    }
+
+    public String retrieveFromMap(String key){
+        List<JsonAlarmStorage> value;
+        System.out.println("retriving json from map");
+        if (map.containsKey(key)) {
+            value = map.get(key);
+            System.out.println(value.size());
+            System.out.println("Key : " + key +" value :"+ value.get(0).alarm+""+value.get(0).json);
+            return value.get(0).json.toString();
+        }
+        return "nothing";
+    }
+
+    public String retrieveAlarmConditionFromMap(String key){
+        List<JsonAlarmStorage> value;
+        System.out.println("retriving alarm condition from map");
+        if (map.containsKey(key)) {
+            value = map.get(key);
+            //System.out.println("Key : " + key +" value :"+ value);
+            return value.get(0).alarm;
+        }
+        return "uuid not found";
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public JSONObject retrieveALLFromMapBatch(){
+        System.out.println("Encoding and retriving json for batch");
+        JSONObject eventList = new JSONObject();
+        JSONArray list = new JSONArray();
+        Iterator i = map.keySet().iterator();
+        while(i.hasNext())
+        {
+            String key = i.next().toString();
+            List<JsonAlarmStorage> value = map.get(key);
+            System.out.println("Key : " + key +" value :"+ value.get(0).alarm+""+value.get(0).json);
+            JSONObject json = value.get(0).json;
+            System.out.println(".........................");
+            System.out.println(json.get("event"));
+            JSONObject obj = (JSONObject) json.get("event");
+            list.add(obj);
+        }
+        System.out.println(".........................");
+        eventList.put("eventList", list);
+        System.out.println(eventList);
+        System.out.println(".........................");
+        return eventList;
+    }
+
+
+    @SuppressWarnings("rawtypes")
+    public void retrieveALLFromMap(){
+        System.out.println("retrive all from map");
+        Iterator i = map.keySet().iterator();
+        String mapValues = null;
+        while(i.hasNext()){
+            String key = i.next().toString();
+            List<JsonAlarmStorage> value = map.get(key);
+        mapValues = "uuid: "+key+" jsonStructure: "+value.get(0).json.toString()+" AlarmCondition: "+value.get(0).alarm+": VesSend status: "+value.get(0).vesSendStatus;	
+        System.out.println(mapValues);
+        }
+    }
+
+    public void deleteFromMap(String key){
+        System.out.println("deleting json from map");
+        map.remove(key);
+        System.out.println("values of key "+key+" deleted successfully");
+    }
+
+    public void deleteAllFromMap(){
+        System.out.println("clearing map");
+        map.clear();
+        System.out.println("map cleared..");
+        System.out.println(map.toString());
+    }
+
+    public int totalEntriesInMap(){
+        System.out.println("map size");
+        return map.size();
+    }
+
+    public boolean isJsonFound(String key){
+        System.out.println("checking uuid in map");
+        if(map.containsKey(key))
+            return true;
+        return false;
+    }
+}
---
+++ vesagent/src/main/java/org/onap/multicloud/openstack/vmware/VsphereEntity.java
@@ -0,0 +1,45 @@
+ /* Copyright (c) 2017 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package org.onap.multicloud.openstack.vmware;
+
+public class VsphereEntity {
+    public String VsphereServerName;
+    public String VsphereUsername;
+    public String VsperePassword;
+    public String VsphereVMname;
+    public String getVsphereUsername() {
+        return VsphereUsername;
+    }
+    public void setVsphereUsername(String vsphereUsername) {
+        VsphereUsername = vsphereUsername;
+    }
+    public String getVsperePassword() {
+        return VsperePassword;
+    }
+    public void setVsperePassword(String vsperePassword) {
+        VsperePassword = vsperePassword;
+    }
+    public String getVsphereVMname() {
+        return VsphereVMname;
+    }
+    public void setVsphereVMname(String vsphereVMname) {
+        VsphereVMname = vsphereVMname;
+    }
+    public String getVsphereServerName() {
+        return VsphereServerName;
+    }
+    public void setVsphereServerName(String vsphereServerName) {
+        VsphereServerName = vsphereServerName;
+    }
+}
