5c912305624ba5794f381b9c026c357c3028cebf
Arun Arora <aroraarun@vmware.com>
2017-09-26 21:57:03 +0530

    VMWare vesagent for FCAPS
    Added code coverage TC and related changes
    
    Change-Id: If4b7f82fe7175a4d018cd5c5232217e98824c0bd
    Issue-ID: MULTICLOUD-8
    Signed-off-by: Arun Arora <aroraarun@vmware.com>

---
+++ vesagent/docker/opt/start-manager.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# TODO: Enable the below command to execute the VESAgent process
+#exec java -jar /opt/vesagent-1.0.0-SNAPSHOT.jar /opt/etc/Agent.properties
+while :; do echo 'sleeping for 3600 secs'; sleep 3600; done
