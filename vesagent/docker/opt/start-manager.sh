965979f8ba0e18da785afa00698c940001916966
Ethan Lynn <ethanlynnl@vmware.com>
2018-01-16 11:50:17 +0800

    Update license for VIO plugin
    Update license
    
    Change-Id: I97cf6e844c5f942ff2a91c7d26a118d0a41fec71
    Issue-ID: MULTICLOUD-141
    Signed-off-by: Ethan Lynn <ethanlynnl@vmware.com>

--- vesagent/docker/opt/start-manager.sh
+++ vesagent/docker/opt/start-manager.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright (c) 2017-2018 VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # TODO: Enable the below command to execute the VESAgent process
 exec java -jar /opt/vesagent-1.0.0.jar /opt/etc/Agent.properties
 #while :; do echo 'sleeping for 3600 secs'; sleep 3600; done
