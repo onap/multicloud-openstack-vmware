267effc7b68178213259243cfafb6d2649403400
liangke <lokyse@163.com>
2018-03-15 16:40:22 +0800

    Fix vio logging format
    Update the logging format.
    Add context middleware to set MDC attributes.
    
    Change-Id: Ie6fc6848f38ebc74312b1831a5bb609f4060b406
    Issue-ID: MULTICLOUD-151
    Signed-off-by: liangke <lokyse@163.com>

--- vio/vio/settings.py
+++ vio/vio/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     #customer middleware
+     'vio.middleware.LogContextMiddleware',
 ]
 
 ROOT_URLCONF = 'vio.urls'
