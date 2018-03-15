43caad57874925f46284cfaa8849d6d86a21a2bf
liangke <lokyse@163.com>
2018-03-15 16:40:22 +0800

    Fix vio logging format
    Update the logging format.
    Add context middleware to set MDC attributes.
    
    Change-Id: Ie6fc6848f38ebc74312b1831a5bb609f4060b406
    Issue-ID: MULTICLOUD-151
    Signed-off-by: liangke <lokyse@163.com>

--- vio/vio/pub/config/config.py
+++ vio/vio/pub/config/config.py
@@ -33,6 +33,10 @@ REDIS_HOST = '127.0.0.1'
 REDIS_PORT = '6379'
 REDIS_PASSWD = ''
 
+# [MDC]
+SERVICE_NAME = "multicloud-vio"
+FORWARDED_FOR_FIELDS = ["HTTP_X_FORWARDED_FOR", "HTTP_X_FORWARDED_HOST",
+                        "HTTP_X_FORWARDED_SERVER"]
 # [mysql]
 # DB_IP = '127.0.0.1'
 # DB_PORT = 3306
