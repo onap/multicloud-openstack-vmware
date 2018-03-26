49c754f12db1d6ac2b86dfa9a4aa55eb56554d18
Ethan Lynn <ethanlynnl@vmware.com>
2018-03-26 11:35:01 -0700

    Add test_get_vim
    Add test_get_vim for AAIClient
    
    Change-Id: Ib51be2adbc8b365b9b195fb8d629381928fc3a1b
    Issue-ID: MULTICLOUD-199
    Signed-off-by: Ethan Lynn <ethanlynnl@vmware.com>

---
+++ vio/vio/tests/test_aai_client.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2018 VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import mock
+import unittest
+
+from vio.pub.utils import restcall
+
+
+class TestAAIClient(unittest.TestCase):
+
+    def setUp(self):
+        self.view = restcall.AAIClient("vmware", "4.0")
+
+    @mock.patch.object(restcall, "call_req")
+    def test_get_vim(self, mock_call):
+        mock_call.return_value = [0, '{"cloudOwner": "vmware"}']
+        ret = self.view.get_vim(get_all=True)
+        expect_ret = {"cloudOwner": "vmware"}
+        self.assertEqual(expect_ret, ret)
