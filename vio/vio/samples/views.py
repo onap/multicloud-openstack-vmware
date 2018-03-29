829b8cf318ea212717d12ce7376a0eeee2c562a8
liangke <lokyse@163.com>
2018-03-29 17:48:43 +0800

    Fetch output of logging
    Change-Id: I9599e838e48103a4f9a21cd3c159a5b2d0a90ce0
    Issue-ID: MULTICLOUD-152
    Signed-off-by: liangke <lokyse@163.com>

--- vio/vio/samples/views.py
+++ vio/vio/samples/views.py
@@ -10,12 +10,14 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
+import os
 import logging
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 logger = logging.getLogger(__name__)
+log_file = "/var/log/onap/multicloud/vio/vio.log"
 
 
 class SampleList(APIView):
@@ -24,5 +26,13 @@ class SampleList(APIView):
     """
 
     def get(self, request, format=None):
+      
         logger.debug("get")
-        return Response({"status": "active"})
+        output = ""
+        if os.path.exists(log_file):
+          
+            with open("/var/log/onap/multicloud/vio/vio.log", "r") as f:
+                lines = f.readlines()
+                output = lines[-1]
+                
+        return Response({"status": "active", "logs": output})
