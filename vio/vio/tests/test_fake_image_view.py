60eec67b9902bd8e96cdb3a9fc1c303c6da3dc66
Bin Sun <bins@vmware.com>
2018-04-08 11:10:01 +0800

    Add fake image test
    Change-Id: I6014c3ee8595dba4a2bd690c78b04885839a1fb4
    Issue-ID: MULTICLOUD-199
    Signed-off-by: Bin Sun <bins@vmware.com>

---
+++ vio/vio/tests/test_fake_image_view.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2018 VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import mock
+import unittest
+
+from vio.swagger.views.fakeplugin.image import views
+from vio.swagger.views.fakeplugin.fakeData import fakeResponse
+
+
+class TestFakeImageDetail(unittest.TestCase):
+
+    def setUp(self):
+        self.view = views.FakeImageDetail()
+
+    @mock.patch.object(fakeResponse, "image_detail")
+    def test_get(self, mock_image_detail):
+        mock_image_detail.return_value = {
+            "id": "1234abcd"
+        }
+        resp = self.view.get(mock.Mock(), "1234abcd")
+        self.assertEqual(200, resp.status_code)
