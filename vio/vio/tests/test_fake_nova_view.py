63a4f2fd7a6cb79096a44cf86cdd9a9ac2d551c7
Bin Sun <bins@vmware.com>
2018-04-08 16:52:35 +0800

    Add fake get server test
    Change-Id: I0c5a612b581240e593417672dcd377316d458959
    Issue-ID: MULTICLOUD-199
    Signed-off-by: Bin Sun <bins@vmware.com>

---
+++ vio/vio/tests/test_fake_nova_view.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2018 VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import mock
+import unittest
+
+from vio.swagger.views.fakeplugin.nova import views
+from vio.swagger.views.fakeplugin.fakeData import fakeResponse
+
+Token = "gAAAAABZmlkS3H24i7446u41QoDMMEFi49sUbYiB2fqrZq00" \
+        "TR92RDLxt4AWzHsBa36IeWeY_eVEnDWAjIuV" \
+        "vK2osp6mPTEKGCvywrksCorunJqPCf46nBhGt-P4" \
+        "bqXMUWRMgowfIS2_kv1pQwvoP00_Rs6KlDaWt-miEu7s24m3En9Qsbg8Ecw"
+
+Server = "764e369e-a874-4401-b7ce-43e4760888da"
+
+
+class TestFakeNovaServer(unittest.TestCase):
+
+    def setUp(self):
+        self.view = views.FakeNovaServer()
+
+    @mock.patch.object(fakeResponse, "show_serverDetail")
+    def test_get_server(self, mock_show_serverDetail):
+        req = mock.Mock()
+        req.META = {
+            "HTTP_X_AUTH_TOKEN": Token
+        }
+        mock_show_serverDetail.return_value = {
+            "server-id": "1234abcd"
+        }
+        resp = self.view.get(req, "abcd", Server)
+        self.assertEqual(200, resp.status_code)
