324fd4acbb6a4b0b7c33cee0661efb58c6304db2
Ethan Lynn <ethanlynnl@vmware.com>
2019-05-14 11:42:44 +0800

    Remove redis server for security reason
    Remove redis server for security reason
    
    Change-Id: Ie4ee851a6f3d8cd137fd3f84536c51d8ebfa407c
    Issue-ID: MULTICLOUD-626
    Signed-off-by: Ethan Lynn <ethanlynnl@vmware.com>

--- vio/docker/instance-run.sh
+++ vio/docker/instance-run.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-service redis-server start
+# service redis-server start
 
 cd ./vio
 ./run.sh
